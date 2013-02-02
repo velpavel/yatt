775a847bf4d5248027496bcb5a0ae66d4128bdf9
PaVel <vel.pavel@gmail.com>
2013-02-03 01:34:18 +0400

    Add def to format duration seconds->human
    Ранее длительность отображалася в секундах. Сечас добавлено процедура дял
    форматирования секунд в человеческий формат: дни, часы, минуты, секунды.

--- yatt/timerecords/views.py
+++ yatt/timerecords/views.py
@@ -6,6 +6,26 @@ from django.template import RequestContext
 from django.utils import timezone
 import datetime
 
+#Процедура для преобразования длительности в секундах в строку
+def format_duration(duration):
+    s=''
+    days=duration//(24*60*60)
+    if days:
+        s=s+'Дней: '+str(days)+'. '
+        duration=duration-(days*24*60*60)
+    hours=duration//(60*60)
+    if hours:
+        s=s+'Часов: '+str(hours)+'. '
+        duration=duration-(hours*60*60)
+    minutes=duration//60
+    if minutes:
+        s=s+'Минут: '+str(minutes)+'. '
+        duration=duration-(minutes*60)
+    seconds=duration//1
+    if seconds:
+        s=s+'Секунд: '+str(seconds)+'.'
+    return s
+
 def project_list(request):
     #Добыча списка корневых проектов.
     projects=Project.objects.filter(user=request.user)
@@ -19,6 +39,7 @@ def project_list(request):
         for rec in recs_list:
             if rec.duration: 
                 duration=duration+rec.duration
+        duration=format_duration(duration)
         projects_list.append({'project': project, 'duration':duration})
     return render_to_response('timerecords/Projects_to_start.html', {'list': projects_list, 'now_going': null_rec_list}, 
                                 context_instance=RequestContext(request)) 
